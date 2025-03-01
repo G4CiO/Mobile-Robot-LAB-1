4503b98e7c947dc003132b35ee7693a5a9578177
G4CiO <86733145+G4CiO@users.noreply.github.com>
2025-03-01 12:17:32 +0700

    Update urdf

--- limo_description/launch/limo.launch.py
+++ limo_description/launch/limo.launch.py
@@ -27,7 +27,7 @@ def generate_launch_description():
     pkg = get_package_share_directory('limo_description')
     # pkg = get_package_share_directory('lab1_robot_description')
 
-    path_description = os.path.join(pkg,'urdf','robot.xacro')
+    path_description = os.path.join(pkg,'urdf','limo_no_meshes.xacro')
     robot_desc_xml = xacro.process_file(path_description).toxml()
     #robot_desc_xml = xacro.process_file(path_description,mappings={'robot_name': namespace}).toxml()
     
